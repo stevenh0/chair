d3f7ac7055f42eab07d071e156cbff25e2f4b76b
Steven Huang <steven.huang@cyclicarx.com>
2018-05-11 23:05:09 -0400

    some parsing things

--- chair/models.py
+++ chair/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class OrderStatus(models.Model):
+    auto_fulfill = models.BooleanField()
+    last_update = models.CharField()
+
+
+class Customer(models.Model):
+    pass
+
+
+class Order(models.Model):
+    status = models.CharField()
+    shipped = models.BooleanField()
+    tracking_id = models.CharField()
+    customer_id = models.ForeignKey('Customer')
--- chair/views.py
+++ chair/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from chair.secrets import SECRET_VARIABLE
-# Create your views here.
+from secrets import BESTBUY_KEY
+import requests
+import json
+
+
 @login_required()
 def console(request):
-	return render(request, "console.html", context={"secret": SECRET_VARIABLE})
+    return render(request, "console.html", context={"secret": 'lol'})
+
+
+def process_order(order_id, accept):
+    headers = {'Authorization': BESTBUY_KEY}
+    acceptance_data = {'order_lines': [{'accepted': accept, 'id': order_id}]}
+    requests.put('https://marketplace.bestbuy.ca/api/orders/{}/accept'.format(order_id),
+                 data=json.dumps(acceptance_data), headers=headers)
+    return 1
+
+
+def update_tracking(order_id, tracking_number):
+    headers = {'Authorization': BESTBUY_KEY}
+    tracking_data = {'carrier_code': 'CPCL', 'tracking_number': tracking_number}
+    requests.put('https://marketplace.bestbuy.ca/api/orders/{}/accept'.format(order_id),
+                 data=json.dumps(tracking_data), headers=headers)
+    return 1
+
+
+# date should be in format yyyy-MM-dd
+def grab_orders(date):
+    headers = {'Authorization': BESTBUY_KEY}
+    r = requests.get('https://marketplace.bestbuy.ca/api/orders', {"start_date": ""}, headers=headers)
+    r_data = json.loads(r)
+    orders = r_data.get('orders')
+    for order in orders:
+        # grab necessary fields for newegg stuff and enter them into the db
+        pass
+    return len(orders)
