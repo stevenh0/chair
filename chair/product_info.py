325a6eca55ae5566a2745474af8185ff0f51def1
steven <stevenh0@hotmail.com>
2018-05-17 19:18:12 -0400

    product info and newegg json

---
+++ chair/product_info.py
@@ -0,0 +1,15 @@
+# Product SKU? : (product name, newegg part #)
+PRODUCT_INFO = {
+	"9SIAFG16RF1304": ("PulseLabz Guardian Series Gaming Chair - Black/Black", G-BK),
+	"9SIAFG16RF1303": ("PulseLabz Guardian Series Gaming Chair - White/Black", G-W),
+	"9SIAFG16RF1302": ("PulseLabz Guardian Series Gaming Chair - Blue/Black", G-B),
+	"9SIAFG16RF1269": ("PulseLabz Guardian Series Gaming Chair - Red/Black", G-R),
+	"9SIAFG16RE9293": ("PulseLabz Enforcer Series Gaming Chair - Black/Black", E-BK),
+	"9SIAFG16RE9292": ("PulseLabz Enforcer Series Gaming Chair - White/Black", E-W),
+	"9SIAFG16RE9291": ("PulseLabz Enforcer Series Gaming Chair - Blue/Black", E-B),
+	"9SIAFG16RE9105": ("PulseLabz Enforcer Series Gaming Chair - Red/Black", E-R),
+	"9SIAFG16RE9054": ("PulseLabz Challenger Series Gaming Chair - Black/Black", C-BK),
+	"9SIAFG16RE8116": ("PulseLabz Challenger Series Gaming Chair - White/Black", C-W),
+	"9SIAFG16RE8111": ("PulseLabz Challenger Series Gaming Chair - Blue/Black", C-B),
+	"9SIAFG16R80209": ("PulseLabz Challenger Series Gaming Chair - Red/Black", C-R),
+}
--- chair/views.py
+++ chair/views.py
@@ -1,38 +1,67 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from secrets import BESTBUY_KEY
-import requests
+from chair.secrets import BESTBUY_KEY
 import json
+import datetime
 
 
 @login_required()
 def console(request):
-    return render(request, "console.html", context={"secret": 'lol'})
+	order_info, customer_info = get_bestbuy_orders()
+	if AUTO_FULFILL:
+		newegg_order = create_newegg_order(customer_info)
+		fulfill_newegg_order(newegg_order)
+	return render(request, "console.html", context={"order_info": order_info, "customer_info": customer_info})
 
 
-def process_order(order_id, accept):
-    headers = {'Authorization': BESTBUY_KEY}
-    acceptance_data = {'order_lines': [{'accepted': accept, 'id': order_id}]}
-    requests.put('https://marketplace.bestbuy.ca/api/orders/{}/accept'.format(order_id),
-                 data=json.dumps(acceptance_data), headers=headers)
-    return 1
+@login_required():
+def newegg_fulfill(request):
+	newegg_order = create_newegg_order(customer_info)
+	fulfill_newegg_order(newegg_order)
 
 
-def update_tracking(order_id, tracking_number):
-    headers = {'Authorization': BESTBUY_KEY}
-    tracking_data = {'carrier_code': 'CPCL', 'tracking_number': tracking_number}
-    requests.put('https://marketplace.bestbuy.ca/api/orders/{}/accept'.format(order_id),
-                 data=json.dumps(tracking_data), headers=headers)
-    return 1
+
+def get_bestbuy_order():
+	headers = { 'Authorization': BESTBUY_KEY}
+	r = requests.get('https://marketplace.bestbuy.ca/api/orders', headers=headers)
+	# grab necessary info and return it, probably some customer information stuff
+	order_info = []
+	customer_info = []
+	return order_info, customer_info
 
 
-# date should be in format yyyy-MM-dd
-def grab_orders(date):
-    headers = {'Authorization': BESTBUY_KEY}
-    r = requests.get('https://marketplace.bestbuy.ca/api/orders', {"start_date": ""}, headers=headers)
-    r_data = json.loads(r)
-    orders = r_data.get('orders')
-    for order in orders:
-        # grab necessary fields for newegg stuff and enter them into the db
-        pass
-    return len(orders)
+# TODO: multiple objects in a single order - what format
+# TODO: what is SalesChannel
+def get_newegg_order(orders, customer):
+	today = datetime.date.today().strftime('%m/%d/%Y')
+	channel = "PulseLabz"
+	shipping = "CAN Ground(2-7 Business Days)"
+	newegg = {
+	  "-xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+	  "Header": { "DocumentVersion": "1.0" },
+	  "MessageType": "MultiChannelOrderCreation",
+	  "Message": {
+	    "MultiChannelOrder": {
+	      "Order": {
+	        "OrderDate": today,
+	        "SalesChannel": channel,
+	        "SellerOrderID": order.id,
+	        "ShippingMethod": shipping,
+	        "ShipToFirstName": customer.first_name,
+	        "ShipToLastName": customer.last_name,
+	        "ShipToAddressLine1": customer.address,
+	        "ShipToCity": customer.city,
+	        "ShipToState": customer.state,
+	        "ShipToPostalCode": customer.postal_code,
+	        "ShipToCountry": customer.county,
+	        "ShipToPhoneNumber": customer.phone,
+	        "ItemList": {
+	          "Item": [{
+	              "SellerPartNumber": order.part_number,
+	              "Quantity": order.quantity
+	            } for order in orders]
+	        }
+	      }
+	    }
+	  }
+	}
