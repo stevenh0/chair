f6e86bedb1c4eff08779089d2d91e3a7c60f4935
Steven Huang <steven.huang@cyclicarx.com>
2018-05-18 16:51:51 -0400

    finished models and parsing of request

--- chair/models.py
+++ chair/models.py
@@ -4,23 +4,27 @@ from django.db import models
 # Create your models here.
 class OrderStatus(models.Model):
     auto_fulfill = models.BooleanField()
-    last_update = models.CharField()
+    last_update = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Customer(models.Model):
-    customer_id = models.CharField()
-    firstname = models.CharField()
-    lastname = models.CharField()
-    county = models.CharField()
-    phone = models.CharField()
-    state = models.CharField()
-    street = models.CharField()
-    zip = models.CharField()
+    customer_id = models.CharField(max_length=100, blank=True, null=True)
+    firstname = models.CharField(max_length=100, blank=True, null=True)
+    lastname = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    street = models.CharField(max_length=100, blank=True, null=True)
+    zip = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Order(models.Model):
-    order_id = models.CharField()
-    status = models.CharField()
-    shipped = models.BooleanField()
-    tracking_id = models.CharField()
-    customer_id = models.ForeignKey('Customer')
+    order_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=100, blank=True, null=True)
+    tracking_id = models.CharField(max_length=100, blank=True, null=True)
+    customer_id = models.ForeignKey('Customer', on_delete=models.CASCADE, blank=True, null=True)
+    quantity = models.CharField(max_length=100, blank=True, null=True)
+    product_name = models.CharField(max_length=100, blank=True, null=True)
+    received = models.CharField(max_length=100, blank=True, null=True)
+    part_number = models.CharField(max_length=100, blank=True, null=True)
--- chair/views.py
+++ chair/views.py
@@ -4,69 +4,77 @@ from secrets import BESTBUY_KEY, CARRIER_CODE
 import requests
 import json
 from chair.models import Order, Customer
+from chair.product_info import PRODUCT_INFO
 import datetime
 
 
+# workflow: load bestbuy orders from last date, grab unshipped orders, display.
+# have option to fulfill order - user clicks fulfill = send newegg shipment, update
+# bestbuy order status
 @login_required()
 def console(request):
-	order_info, customer_info = get_bestbuy_orders()
-	if AUTO_FULFILL:
-		newegg_order = create_newegg_order(customer_info)
-		fulfill_newegg_order(newegg_order)
-	return render(request, "console.html", context={"order_info": order_info, "customer_info": customer_info})
+    return render(request, "console.html", context={"order_info": Order.objects.all()[:10]})
 
 
-@login_required():
-def newegg_fulfill(request):
-	newegg_order = create_newegg_order(customer_info)
-	fulfill_newegg_order(newegg_order)
+@login_required()
+def newegg_fulfill(request, order_id):
+    order = Order.objects.filter(order_id=order_id)
+    for o in order:
+        newegg_order = get_newegg_order(o)
+        newegg_ship(newegg_order)
 
+
+# TODO: actual newegg request here
+def newegg_ship(newegg_json):
+    pass
 
 
 def get_bestbuy_order():
-	headers = { 'Authorization': BESTBUY_KEY}
-	r = requests.get('https://marketplace.bestbuy.ca/api/orders', headers=headers)
-	# grab necessary info and return it, probably some customer information stuff
-	order_info = []
-	customer_info = []
-	return order_info, customer_info
+    headers = {'Authorization': BESTBUY_KEY}
+    r = requests.get('https://marketplace.bestbuy.ca/api/orders', headers=headers)
+    # grab necessary info and return it, probably some customer information stuff
+    order_info = []
+    customer_info = []
+    return order_info, customer_info
 
 
 # TODO: multiple objects in a single order - what format
 # TODO: what is SalesChannel
-def get_newegg_order(orders, customer):
-	today = datetime.date.today().strftime('%m/%d/%Y')
-	channel = "PulseLabz"
-	shipping = "CAN Ground(2-7 Business Days)"
-	newegg = {
-	  "-xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-	  "Header": { "DocumentVersion": "1.0" },
-	  "MessageType": "MultiChannelOrderCreation",
-	  "Message": {
-	    "MultiChannelOrder": {
-	      "Order": {
-	        "OrderDate": today,
-	        "SalesChannel": channel,
-	        "SellerOrderID": order.id,
-	        "ShippingMethod": shipping,
-	        "ShipToFirstName": customer.first_name,
-	        "ShipToLastName": customer.last_name,
-	        "ShipToAddressLine1": customer.address,
-	        "ShipToCity": customer.city,
-	        "ShipToState": customer.state,
-	        "ShipToPostalCode": customer.postal_code,
-	        "ShipToCountry": customer.county,
-	        "ShipToPhoneNumber": customer.phone,
-	        "ItemList": {
-	          "Item": [{
-	              "SellerPartNumber": order.part_number,
-	              "Quantity": order.quantity
-	            } for order in orders]
-	        }
-	      }
-	    }
-	  }
-	}
+def get_newegg_order(order):
+    customer = order.customer_id
+    today = datetime.date.today().strftime('%m/%d/%Y')
+    channel = "PulseLabz"
+    shipping = "CAN Ground(2-7 Business Days)"
+    newegg = {
+        "-xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "Header": { "DocumentVersion": "1.0" },
+        "MessageType": "MultiChannelOrderCreation",
+        "Message": {
+            "MultiChannelOrder": {
+                "Order": {
+                    "OrderDate": today,
+                    "SalesChannel": channel,
+                    "SellerOrderID": order.order_id,
+                    "ShippingMethod": shipping,
+                    "ShipToFirstName": customer.firstname,
+                    "ShipToLastName": customer.lastname,
+                    "ShipToAddressLine1": customer.street,
+                    "ShipToCity": customer.city,
+                    "ShipToState": customer.state,
+                    "ShipToPostalCode": customer.zip,
+                    "ShipToCountry": customer.country,
+                    "ShipToPhoneNumber": customer.phone,
+                    "ItemList": {
+                        "Item": {"SellerPartNumber": order.part_number,
+                                 "Quantity": order.quantity}
+                    }
+                }
+            }
+        }
+    }
+    return newegg
+
+
 def update_tracking(order_id, tracking_number):
     headers = {'Authorization': BESTBUY_KEY}
     tracking_data = {'carrier_code': CARRIER_CODE, 'tracking_number': tracking_number}
@@ -76,37 +84,48 @@ def update_tracking(order_id, tracking_number):
 
 
 # date should be in format yyyy-MM-dd
-def grab_orders(date):
+def grab_orders(date=None):
     headers = {'Authorization': BESTBUY_KEY}
-    r = requests.get('https://marketplace.bestbuy.ca/api/orders', {"start_date": date}, headers=headers)
-    r_data = json.loads(r)
+    r = requests.get('https://marketplace.bestbuy.ca/api/orders', {"start_date": date, "paginate": False}, headers=headers)
+    r_data = json.loads(r.content)
     orders = r_data.get('orders')
     for order in orders:
         # grab necessary fields for newegg stuff and enter them into the db
         load_order(order)
-        pass
     return len(orders)
 
-# TODO: how does newegg know what product to ship
+
 # fill in information needed for an order
 def load_order(order_info):
-    order, created = Order.objects.get_or_create(order_id=order_info.get('order_id'))
-    customer = update_customer_info(order_info.get('customer'))
-    order.customer = customer
-    status = order_info.get('order_state')
+    for item in order_info.get('order_lines'):
+        product_name = item.get('product_title')
+        order, created = Order.objects.get_or_create(order_id=order_info.get('order_id'), product_name=product_name)
+        customer = update_customer_info(order_info.get('customer'))
+        order.customer_id = customer
+        order.status = order_info.get('order_state')
+        order.tracking_id = order_info.get('shipping_tracking')
+        order.quantity = item.get('quantity')
+        order.received = order_info.get('created_date')
+        try:
+            order.part_number = PRODUCT_INFO.get(product_name)[1]
+        except:
+            pass
+        order.save()
 
 
-# TODO: do we need billing address? assuming no
 # TODO: what do condo addresses look like
 def update_customer_info(customer_info):
     customer, created = Customer.objects.get_or_create(customer_id=customer_info.get('customer_id'))
     customer.firstname = customer_info.get('firstname')
     customer.lastname = customer_info.get('lastname')
-    customer.country = customer_info['shipping_adress'].get('country')
-    customer.city = customer_info['shipping_adress'].get('city')
-    customer.phone = customer_info['shipping_adress'].get('phone')
-    customer.state = customer_info['shipping_adress'].get('state')
-    customer.street = customer_info['shipping_adress'].get('street_1')
-    customer.zip = customer_info['shipping_adress'].get('zip_code')
+    try:
+        customer.country = customer_info['shipping_address'].get('country')
+        customer.city = customer_info['shipping_address'].get('city')
+        customer.phone = customer_info['shipping_address'].get('phone')
+        customer.state = customer_info['shipping_address'].get('state')
+        customer.street = customer_info['shipping_address'].get('street_1')
+        customer.zip = customer_info['shipping_address'].get('zip_code')
+    except AttributeError:
+        print('could not parse customer {}, {}'.format(customer.customer_id, customer_info))
     customer.save()
     return customer
