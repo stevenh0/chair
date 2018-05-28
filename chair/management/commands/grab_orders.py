41623d4bf373c6f13657b79ee51fc10b2a01edd3
Steven Huang <steven.huang@cyclicarx.com>
2018-05-27 23:56:50 -0400

    mgmt comand and slight changes to order accept behavior

--- chair/management/commands/grab_orders.py
+++ chair/management/commands/grab_orders.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
-from chair.order_processing.bestbuy import grab_orders
+from chair.order_processing.bestbuy import grab_orders, process_order
+from chair.models import Order
 
 
 class Command(BaseCommand):
     def handle(self, *app_labels, **options):
         grab_orders()
+        for order in Order.objects.filter(status='WAITING_ACCEPTANCE'):
+            process_order(order.order_id, True)
