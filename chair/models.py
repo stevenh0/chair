d149b383335927e5ea35c45755d2279a62fd33df
stevenh0 <stevenh0@hotmail.com>
2019-02-03 23:05:56 -0500

    Update models.py

--- chair/models.py
+++ chair/models.py
@@ -30,7 +30,7 @@ class Order(models.Model):
     carrier_code = models.CharField(max_length=200, blank=True, null=True)
     customer_id = models.ForeignKey('Customer', on_delete=models.CASCADE, blank=True, null=True)
     quantity = models.CharField(max_length=100, blank=True, null=True)
-    product_name = models.CharField(max_length=100, blank=True, null=True)
+    product_name = models.CharField(max_length=500, blank=True, null=True)
     received = models.CharField(max_length=100, blank=True, null=True)
     part_number = models.CharField(max_length=100, blank=True, null=True)
     newegg_feed = models.CharField(max_length=100, blank=True, null=True)
